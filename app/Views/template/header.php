72935f6e6644428fc9351df9371a2ade7a1a8332
Hélvio Filho <helviosvf@gmail.com>
2022-08-09 23:13:20 -0300

    Fix: changed path icon

--- app/Views/template/header.php
+++ app/Views/template/header.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <title>A Hora do Anjo</title>
-<link rel="shortcut icon" type="image/x-icon" href="<?=base_url(['icone','angel.svg']) ?>">
+<link rel="shortcut icon" type="image/x-icon" href="<?=base_url(['icon','angel.svg']) ?>">
 <link rel="stylesheet" type="text/css" href="<?=base_url(['assets','css','bootstrap.css']) ?>">
 <?=$uri->getSegment(1) == "publicacao" ? '<link href="'.base_url(['assets','css','quill.css']).'" rel="stylesheet">' : '' ?>
 <link rel="stylesheet" type="text/css" href="<?=base_url(['assets','css','style.css']) ?>">
