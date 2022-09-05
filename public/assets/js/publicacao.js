5b68e789b01d82bb287e5e526357ea72ee4bcc27
Hélvio Filho <helviosvf@gmail.com>
2022-09-05 03:50:07 -0300

    Chore: remove useless tools

--- public/assets/js/publicacao.js
+++ public/assets/js/publicacao.js
@@ -75,11 +75,10 @@ window.onload = function () {
 	var toolbarOptions =
 		[
 			['bold', 'italic', 'underline', 'strike'],
-			[{ 'header': 2 }],
+			[{ 'header': 1 }],
 			[{ 'list': 'ordered' }, { 'list': 'bullet' }],
-			[{ 'indent': '-1' }, { 'indent': '+1' }],
 			[{ 'size': ['small', false, 'large'] }],
-			[{ 'header': [2, 3, 4, 5, 6, false] }],
+			[{ 'header': [1, 2, 3, 4, 5, 6, false] }],
 			[{ 'align': [] }],
 			[{ 'color': [] }],
 			['link', 'image'],
